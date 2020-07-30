a793eabba6bcdb33bf536d3f83f889bda89c0246
Vovaprymachuk <vovaprimachuk@gmail.com>
2020-07-30 21:13:25 +0300

    Login and Registration

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='posts_list_url'),
+    path('register/', register_page, name='register_page_url'),
+    path('login/', login_page, name='login_url'),
+    path('logout/', logout_user, name='logout_url'),
     path('post/<str:slug>/', post_detail, name='post_detail_url'),
     path('tags/', tags_list, name='tags_list_url'),
     path('tags/<str:slug>', tag_detail, name='tag_detail_url'),
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 from blog.models import Post, Tag
+from blog.forms import CreateUserForm
 
 
+def register_page(request):
+    if request.user.is_authenticated:
+        return redirect('posts_list_url')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for {}'.format(user))
+                return redirect(login_page, messages)
+
+        context = {'form': form}
+        return render(request, 'blog/register.html', context)
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('posts_list_url')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('posts_list_url')
+            else:
+                messages.info(request, 'Username OR Password is not correct!')
+
+        context = {}
+        return render(request, 'blog/login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login_url')
+
+
+@login_required(login_url='login_url')
 def posts_list(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'blog/index.html', context)
 
+
+@login_required(login_url='login_url')
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
     context = {'post': post}
     return render(request, 'blog/post_detail.html', context)
 
+
+@login_required(login_url='login_url')
 def tags_list(request):
     tags = Tag.objects.all()
     context = {'tags': tags}
     return render(request, 'blog/tags_list.html', context)
 
+
+@login_required(login_url='login_url')
 def tag_detail(request, slug):
     tag = Tag.objects.get(slug__iexact=slug)
     context = {'tag': tag}
